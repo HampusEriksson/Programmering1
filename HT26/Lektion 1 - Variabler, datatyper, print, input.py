f4de0e0a58e9e7dd4b33a33c8eb0a18fd0a75226
Hampus Eriksson <hampuse1@gmail.com>
2026-08-23 15:34:56 +0200

    prepp

---
+++ HT26/Lektion 1 - Variabler, datatyper, print, input.py
@@ -0,0 +1,23 @@
+# Datatyper - str - text
+
+
+# Variabler
+# variabel_namn = värde
+
+# input
+
+
+# Datatyper - int - heltal
+
+# Ökar age med 1
+
+
+# Datatyper - float - decimaltal
+
+
+# print
+
+# input
+
+# formattera string
+# {}
---
+++ HT26/Lektion 2 - Matematiska operatorer.py
@@ -0,0 +1,31 @@
+# Omvandla input till int
+
+# length = int(input("How tall are you?      "))
+
+# print("Welcome!")
+# print(f"You are {length} cm tall.")
+
+# Addition
+
+
+# Subtraktion
+
+# Multiplikation
+
+# Division
+
+# Upphöjt i - **
+
+# 3 upphöjt i 5
+
+
+# Roten ur
+
+
+# Floor division
+
+
+# Modulo
+
+
+# Udda tal & Jämna tal
