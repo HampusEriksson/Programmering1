958295a46603613efa26638f1b7992a3d6a04de8
Hampus Eriksson <90573586+HampusEriksson@users.noreply.github.com>
2026-08-24 12:45:44 +0200

    L1

--- HT26/Lektion 1 - Variabler, datatyper, print, input.py
+++ HT26/Lektion 1 - Variabler, datatyper, print, input.py
@@ -1,23 +1,34 @@
 # Datatyper - str - text
-
+"Hampus"
+'Hampus'
 
 # Variabler
 # variabel_namn = värde
-
-# input
-
+namn = "Hampus"
+ort = "Solna"
+namn = "Hampus Eriksson"
+# print - skriv ut
+print("Welcome to the program.")
+print(namn)
 
 # Datatyper - int - heltal
+7
+age = 32
+print(age)
 
-# Ökar age med 1
-
+score = 0
+# Öka int med 1
+score = score + 1
+score += 1
+score += 0.5
 
 # Datatyper - float - decimaltal
-
-
-# print
+58.39
 
 # input
+username = input("Username: ")
+user_age = int(input("Age: "))
 
 # formattera string
 # {}
+print(f"Välkommen till programmet {username}. Du är {user_age} år gammal.")
